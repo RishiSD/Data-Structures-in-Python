97296d07ff82b25315e826531882acc99f18da68
rishid <rishideorukhkar@gmail.com>
2018-08-05 17:23:29 +0530

    Adding code for Stack implementation

--- Stack.py
+++ Stack.py
@@ -1,1 +1,13 @@
 # Stack implementation in Python
+from LinkedList import LinkedList
+
+class Stack(object):
+	def __init__(self, top=None):
+		self.ll = LinkedList(top)
+	
+	def push(self, new_element):
+		self.ll.push_front(new_element)
+	
+	def pop(self):
+		popped_element = self.ll.pop_front()
+		return popped_element.value if popped_element else None
