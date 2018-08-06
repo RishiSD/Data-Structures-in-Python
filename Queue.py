f62ee8915c12242d3f2431a69506fd8c95941b7b
rishid <rishideorukhkar@gmail.com>
2018-08-06 12:55:41 +0530

    Adding docstring to Queue.py

--- Queue.py
+++ Queue.py
@@ -2,15 +2,45 @@
 from LinkedList import LinkedList
 
 class Queue(object):
+	"""
+	This class represents a Queue implementation with a LinkedList.
+	
+	Attributes:
+		ll : LinkedList used for implementing the Queue.
+	"""
 	
 	def __init__(self, head=None):
+		"""
+		The constructor for class Queue.
+		
+		Parameters:
+			head : Represents the first element to enqueue.
+		"""
 		self.ll = LinkedList(head)
 		
 	def enqueue(self, new_element):
+		"""
+		The function to enqueue a new element.
+		
+		Parameters:
+			new_element : New element to enqueue.
+		"""
 		self.ll.push_back(new_element)
 		
 	def dequeue(self):
+		"""
+		The function to dequeue a new element.
+		
+		Returns:
+			element : Dequeued element from the head of the queue.
+		"""
 		return self.ll.pop_front()
 		
 	def peek(self):
+		"""
+		The function to get value of element at the beginning of the queue.
+		
+		Parameters:
+			value : Value of element at the beginning of the queue.
+		"""
 		return self.ll.get_position(1)
